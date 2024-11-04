77cf2ec46eb06d54be7e1a6fd466dc00a5a31e63
Nico <dev.nfracchia@gmail.com>
2024-11-03 22:08:17 -0700

    fixing error message display

--- src/api/newsletter.js
+++ src/api/newsletter.js
@@ -7,9 +7,27 @@ export async function sendEmailSubscription(email) {
         return (response.data.status === "subscribed") ? {type: 'success',message:'Thank you for subscribing to In The Know YYC newsletters.'} : {type: 'error',message:'There was an error in the subscription. Please, try again later.'}
     } catch (error) {
         console.error('Error sending email:', error);
-        const message = (error.response.data.message) ? error.response.data.message : 'There was an error in the subscription. Please, try again later.';
+        
+        const message = (error.response.data.message) ? parseErrorMessage(error.response.data.message) : 'There was an error in the subscription. Please, try again later.';
+        
         return {type: 'error', message: message};
     }
 }
 
 export default sendEmailSubscription;
+
+export function parseErrorMessage(text){
+    if(text){
+        const p1 = text.split('{');
+        if(p1[1]){
+            const p2 = p1[1].split('}');
+            if(p2[0]){
+                const info = JSON.parse("{"+p2[0]+"}");
+                if(info.title){
+                    return info.title;
+                }
+            }
+        }
+    }
+    return text;
+}
