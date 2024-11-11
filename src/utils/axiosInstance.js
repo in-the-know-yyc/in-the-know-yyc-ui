45f9406e1f041b3b601e5949ddcae925b0fbf1f3
nicolasfracchia <nfracchia@hotmail.com>
2024-11-11 16:16:17 -0700

    Login fetching token and storing in localstorage. axios interceptor for headers using token

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,25 @@
+import { redirect } from 'next/navigation';
+import axios from 'axios';
+
+const AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+});
+
+AxiosInstance.interceptors.request.use(
+  (config) => {
+      const token = localStorage.getItem('authToken');
+      const accessToken = JSON.parse(token);
+
+      // If token is present, add it to request's Authorization Header
+      if (accessToken) {
+          if (config.headers) config.headers.token = accessToken;
+      }
+      return config;
+  },
+  (error) => {
+      // Handle request errors here
+      return Promise.reject(error);
+  }
+);
+  
+export default AxiosInstance;
