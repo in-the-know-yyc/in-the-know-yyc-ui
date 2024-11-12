ac9db4fef86a13ca099e2ac2834de48d192def42
nicolasfracchia <nfracchia@hotmail.com>
2024-11-11 17:34:51 -0700

    Commented axios interceptor

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
-});
+// const AxiosInstance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL
+// });
 
-AxiosInstance.interceptors.request.use(
-  (config) => {
-      const token = localStorage.getItem('authToken');
-      const accessToken = JSON.parse(token);
+// // AxiosInstance.interceptors.request.use(
+// //   (config) => {
+// //       const token = localStorage.getItem('authToken');
+// //       const accessToken = JSON.parse(token);
 
-      // If token is present, add it to request's Authorization Header
-      if (accessToken) {
-          if (config.headers) config.headers.token = accessToken;
-      }
-      return config;
-  },
-  (error) => {
-      // Handle request errors here
-      return Promise.reject(error);
-  }
-);
+// //       // If token is present, add it to request's Authorization Header
+// //       if (accessToken) {
+// //           if (config.headers) config.headers.token = accessToken;
+// //       }
+// //       return config;
+// //   },
+// //   (error) => {
+// //       // Handle request errors here
+// //       return Promise.reject(error);
+// //   }
+// // );
   
-export default AxiosInstance;
+// export default AxiosInstance;
