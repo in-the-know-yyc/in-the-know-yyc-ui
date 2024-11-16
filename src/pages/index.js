0640a785da470b5dd759407479b75f7f26129bcf
nicolasfracchia <nfracchia@hotmail.com>
2024-11-16 11:57:48 -0700

    Removed sponsors from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ latestEvents }) {
       <LatestEvents latestEvents={latestEvents} />
       <Comments />
       <Community />
-      <Sponsors />
+      {/* <Sponsors /> */}
     </>
   );
 }
